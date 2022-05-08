975581d2a6a1bce20c3ec725112d0363defa7567
Matthew Wilkins <wilkins52matthew@gmail.com>
2022-05-08 07:33:57 -0400

    add comments for clarification inside of meme component

--- src/Meme.js
+++ src/Meme.js
@@ -2,7 +2,7 @@ import React from 'react';
 import memeData from './memes-data'; 
 
 export default function Meme(){
-
+      // Creating a "meme" object and setting it to state
       const [meme, setMeme] = React.useState(
         {
           topText: '',
@@ -11,12 +11,16 @@ export default function Meme(){
         }
       );
 
-
+      // adding the memeData to a variable "allMemeImages"
       const [allMemeImages, setAllMemeImages] = React.useState(memeData);
-
+      // getting a random number based on length of meme urls
       let randNum = Math.floor(Math.random() * allMemeImages.data.memes.length);
+      // using random number to access random url inside of memes array
       let randURL = allMemeImages.data.memes[randNum].url;
-
+      // event handler function to update/set meme object, notice the spread(...)
+      // operator to unpack the *current* meme state object THEN change the 
+      //randomImage property-not JUST update the randomImage property like one may
+      //have done in class components in React! 
       function generateMeme() {
         setMeme(currentMeme => {
           return {
